4252525bd01d845f153446cd786173863b26ac14
t3rmin41 <t33rmin41@gmail.com>
2018-08-08 14:24:15 +0300

    Shape drawing template, see https://stackoverflow.com/questions/31207624/fabricjs-draw-line-but

--- src/main/webapp/app/controllers/canvas.controller.js
+++ src/main/webapp/app/controllers/canvas.controller.js
@@ -23,15 +23,42 @@
 //      console.log('mouse move:', options.e.clientX, options.e.clientY);
 //    });
     
+    // https://stackoverflow.com/questions/31207624/fabricjs-draw-line-but
+    
     ctrl.$onInit = function() {
-      var rect = new fabric.Rect({
-        left: 100,
-        top: 100,
-        fill: 'red',
-        width: 20,
-        height: 20
+//      var rect = new fabric.Rect({
+//        left: 100,
+//        top: 100,
+//        fill: 'red',
+//        width: 20,
+//        height: 20
+//      });
+//      canvas.add(rect);
+
+      var path = new fabric.Path('M 50 50 L 150 50 L 150 260 L 70 250',
+        {
+          stroke: 'blue',
+          strokeWidth: 3,
+          fill: 'transparent'
+        }
+      );
+      canvas.add(path);
+      
+      var polyline = new fabric.Polyline([
+        { x: 40, y: 40 },
+        { x: 150, y: 40 },
+        { x: 150, y: 150 },
+        { x: 50, y: 150 },
+        { x: 40, y: 50 }
+      ], {
+      fill: 'transparent',
+      borderScaleFactor: 1,
+      stroke: 'red',
+      strokeWidth: 3,
+      left: 220,
+      top: 150
       });
-      canvas.add(rect);
+      canvas.add(polyline);
     };
     
   }
